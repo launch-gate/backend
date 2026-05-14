5230fd9c413585d21b959d0aac43241478f256cc
vitaliykostin <vitaliy10kostin@mail.ru>
2026-05-14 18:05:43 +0300

    изменение rest client

--- launch-gate-app/src/main/java/com/launchgate/app/RestClientConfiguration.java
+++ launch-gate-app/src/main/java/com/launchgate/app/RestClientConfiguration.java
@@ -1,7 +1,9 @@
 package com.launchgate.app;
 
+import java.net.http.HttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,7 +11,12 @@ public class RestClientConfiguration {
 
     @Bean
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
+        return RestClient.builder()
+                .requestFactory(new JdkClientHttpRequestFactory(httpClient));
     }
 
     @Bean
